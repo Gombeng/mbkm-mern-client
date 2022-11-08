f7c5c94fe2775b27b7e6b9636d18ec8aaf8fc0d9
gombeng <msrgombeng@gmail.com>
2022-11-08 18:58:19 +0700

    fixing some code

--- src/pages/isiBorang/IsiBorang.js
+++ src/pages/isiBorang/IsiBorang.js
@@ -1,68 +1,78 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet';
 import ClipLoader from 'react-spinners/ClipLoader';
 import { Button, FlexBox } from './../../components/Components';
 import styled from 'styled-components';
-
-const options = [
-	{ label: 'Pemrograman Website', value: 'pemweb' },
-	{ label: 'Analisis Desain Berorientasi Objek', value: 'adbo' },
-	{ label: 'Jaringan Komputer', value: 'jarkom' },
-];
-
-const subjects = [
-	{
-		id: 1,
-		name: 'Perancangan dan Analisis Algoritma',
-		code: 'INF11011',
-		cpl: [
-			{
-				id: 1,
-				code: 'S8',
-				name: 'Menunjukkan sikap bertanggungjawab atas pekerjaan di bidang keahliannya secara mandiri',
-			},
-			{
-				id: 2,
-				code: 'S11',
-				name: 'Memiliki Tekad/Kesungguhan dalam mencapai hasil yang maksimal',
-			},
-			{
-				id: 3,
-				code: 'KU1',
-				name: 'Mampu menerapkan pemikiran logis, kritis, sistematis, dan inovatif dalam konteks pengembangan atau implementasi ilmu pengetahuan dan teknologi yang memperhatikan dan menerapkan nilai humaniora yang sesuai dengan bidang keahliannya',
-			},
-		],
-	},
-	{
-		id: 2,
-		name: 'Pemrograman Website',
-		code: 'INF11011',
-		cpl: [
-			{
-				id: 1,
-				code: 'S8',
-				name: 'Menunjukkan sikap bertanggungjawab atas pekerjaan di bidang keahliannya secara mandiri',
-			},
-			{
-				id: 2,
-				code: 'S11',
-				name: 'Memiliki Tekad/Kesungguhan dalam mencapai hasil yang maksimal',
-			},
-			{
-				id: 3,
-				code: 'KU1',
-				name: 'Mampu menerapkan pemikiran logis, kritis, sistematis, dan inovatif dalam konteks pengembangan atau implementasi ilmu pengetahuan dan teknologi yang memperhatikan dan menerapkan nilai humaniora yang sesuai dengan bidang keahliannya',
-			},
-		],
-	},
-];
+import axios from 'axios';
 
 const IsiBorang = () => {
-	let mhsInfo = JSON.parse(localStorage.getItem('mhsInfo'));
+	const mhsInfo = JSON.parse(localStorage.getItem('mhsInfo'));
 
 	const [loading, setLoading] = useState(false);
+	const [subject, setSubject] = useState('');
+	const [data, setData] = useState('');
+	const [cpmk, setCpmk] = useState('');
 
-	// todo: get api berupa nama mk, lalu ditampilkan dalam bentuk option
+	// console.log(subject);
+
+	useEffect(() => {
+		(async () => {
+			const config = {
+				headers: {
+					'Content-type': 'application/json',
+				},
+			};
+
+			// api for display all matkul
+			const { data } = await axios.get(
+				`http://localhost:8910/api/admin/getAll/cpmks`,
+				config
+			);
+
+			// api for display matkul by id
+			const { matkul } = await axios.get(
+				`http://localhost:8910/api/admin/getAll/cpmks/${subject}`,
+				config
+			);
+
+			console.log(matkul);
+
+			// console.log(data.data);
+			setData(data.data);
+
+			// localStorage.setItem('subjectInfo', JSON.stringify(data));
+		})();
+	}, []);
+
+	const submitHandler = async (e) => {
+		try {
+			const config = {
+				headers: {
+					'Content-type': 'application/json',
+				},
+			};
+
+			setLoading(true);
+
+			// const { data } = await axios.post(
+			// 	`http://localhost:8910/api/admin/input-rps/${subject}`,
+			// 	{
+			// 		_cpmks: cpmk,
+			// 		name,
+			// 	},
+			// 	config
+			// );
+
+			console.log(data.data);
+
+			// localStorage.setItem('adminInfo', JSON.stringify(data.data));
+			setLoading(false);
+		} catch (error) {
+			setLoading(false);
+			console.log(error.response);
+			// setError(error.response.data.message);
+		}
+	};
 
 	return (
 		<div>
@@ -72,43 +82,45 @@ const IsiBorang = () => {
 
 			<FlexBox>
 				<h2 className="mb-1">Silahkan Pilih Mata Kuliah</h2>
-
-				<div className="select-container">
-					<Select>
-						{options.map((option) => (
-							<option value={option.value}>{option.label}</option>
-						))}
-					</Select>
-				</div>
 			</FlexBox>
 
 			<hr className="mb-1" />
 
-			{/* // todo: buat form macam di desain figma */}
+			<form className="mb-1" onSubmit={submitHandler}>
+				<Select
+					className="mb-1"
+					value={subject}
+					onChange={(e) => setSubject(e.target.value)}
+				>
+					<option value="">-- Pilih Mata Kuliah --</option>
+					{data.length > 0 &&
+						data.map(({ _id, name }) => <option value={_id}>{name}</option>)}
+				</Select>
 
-			<form action="" className="mb-1">
-				{subjects.map(({ name, cpl }, index) => (
-					<div key={index}>
-						<h3>{name}</h3>
+				{data.length > 0 &&
+					data.map(({ name, _cpmks }, index) => (
+						<div key={index}>
+							<h3>{name}</h3>
 
-						{cpl.map(({ name, code }, index) => (
-							<div className="mb-1">
-								<strong>Kode CPL - {code}</strong>
-								<p>{name}</p>
-								<Textarea
-									style={{ width: '100%' }}
-									rows="5"
-									placeholder="Jawaban Anda..."
-								></Textarea>
-							</div>
-						))}
-					</div>
-				))}
+							{_cpmks.map(({ name, code }, index) => (
+								<div>
+									<p>{code}</p>
+									<p>{name}</p>
+									<Textarea
+										value={cpmk}
+										onChange={(e) => setCpmk(e.target.value)}
+										rows="5"
+										placeholder="Jawaban anda..."
+									></Textarea>
+								</div>
+							))}
+						</div>
+					))}
 			</form>
-			<div>
+			<div className="mb-1">
 				<Button
-					title={loading ? <ClipLoader size={20} /> : 'Masuk'}
-					className="button mr-1"
+					title={loading ? <ClipLoader size={20} /> : 'Simpan'}
+					className="button mr-1 mb-1"
 					type="submit"
 				/>
 			</div>
@@ -119,12 +131,14 @@ const IsiBorang = () => {
 export default IsiBorang;
 
 const Select = styled.select`
-	padding: 0.8rem 1rem;
+	padding: 1rem;
 	border-radius: 0.3rem;
+	width: 100%;
 `;
 
 const Textarea = styled.textarea`
 	padding: 0.8rem 1rem;
 	border-radius: 0.3rem;
 	resize: vertical;
+	width: 100%;
 `;
